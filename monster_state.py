1047255cebcd92474ce366685c6cdeea9ab98726
kywon22 <kywon622@naver.com>
2022-10-24 01:50:58 +0900

    2주차 몬스터 랜덤 이동

--- monster_state.py
+++ monster_state.py
@@ -1,43 +1,22 @@
 from pico2d import *
+import random
 
 stage_WIDTH, stage_HEIGHT = 700, 650
 
 def handle_events():
     global running
-    global dir1, dir2
-    global direct
-
+    global x, y
     events = get_events()
     for event in events:
         if event.type == SDL_QUIT:
             running = False
-        elif event.type == SDL_KEYDOWN:
-            if event.key == SDLK_RIGHT:
-                dir1 += 1
-                direct = 0
-            elif event.key == SDLK_LEFT:
-                dir1 -= 1
-                direct = 1
-            if event.key == SDLK_UP:
-                dir2 += 1
-                direct = 2
-            elif event.key == SDLK_DOWN:
-                dir2 -= 1
-                direct = 3
-            elif event.key == SDLK_ESCAPE:
-                running = False
-        elif event.type == SDL_KEYUP:
-            if event.key == SDLK_RIGHT:
-                dir1 -= 1
-            elif event.key == SDLK_LEFT:
-                dir1 += 1
-            if event.key == SDLK_UP:
-                dir2 -= 1
-            elif event.key == SDLK_DOWN:
-                dir2 += 1
+        # elif event.type == SDL_MOUSEMOTION:
+        #     x, y = event.x, TUK_HEIGHT - 1 - event.y
+        elif event.type == SDL_KEYDOWN and event.key == SDLK_ESCAPE:
+            running = False
     pass
 
-open_canvas()
+open_canvas(stage_WIDTH, stage_HEIGHT)
 stage_ground = load_image('stay stage.png')
 character = load_image('stage1-1 monster1.png')
 
@@ -48,15 +27,49 @@ direct = 2
 frame = 0
 dir1, dir2 = 0, 0
 
+running = True
+sx, sy = stage_WIDTH // 2, stage_HEIGHT // 2
+x, y = sx, sy
+ax, ay = x, y
+ro = 0
+frame = 0
+hide_cursor()
+
+t = 0
+def reset_world():
+    global ax, ay
+    global t
+    global sx, sy
+
+    ax, ay = random.randint(0, stage_WIDTH), random.randint(0, stage_HEIGHT)
+    t = 0
+    sx, sy = x, y
+    pass
+
+def update_world():
+    global x, y
+    global t
+    global ro
+    t += 0.005
+    x = (1 - t) * sx + t * ax
+    y = (1 - t) * sy + t * ay
+    if ax>sx:
+        ro=1
+    if ax<sx:
+        ro=0
+    if t >= 1.0:
+        reset_world()
+    pass
+
+reset_world()
+
 while running:
+    update_world()
     clear_canvas()
     stage_ground.draw(stage_WIDTH // 2, stage_HEIGHT // 2)
     character.clip_draw(frame * 30, 30 * direct, 20, 20, x, y)
     update_canvas()
     handle_events()
     frame = (frame + 1) % 3
-    x += dir1 * 5
-    y += dir2 * 5
-    delay(0.04)
 
 close_canvas()
